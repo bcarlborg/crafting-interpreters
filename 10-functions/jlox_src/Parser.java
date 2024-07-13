8b81a14b026de76138e4148619fce675b2317cbf
bcarlborg <bcarlborg@gmail.com>
2024-07-13 07:19:18 -0700

    10.5

--- 10-functions/jlox_src/Parser.java
+++ 10-functions/jlox_src/Parser.java
@@ -109,6 +109,7 @@ public class Parser {
     private Stmt statement() {
         if (match(FOR)) return forStatement();
         if (match(PRINT)) return printStatement();
+        if (match(RETURN)) return returnStatement();
         if (match(WHILE)) return whileStatement();
         if (match(IF)) return ifStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
@@ -244,6 +245,17 @@ public class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON)) {
+            value = expression();
+        }
+
+        consume(SEMICOLON, "Expect ';' after return value.");
+        return new Stmt.Return(keyword, value);
     }
 
     private Stmt expressionStatement() {
